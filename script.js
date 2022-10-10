981d8ff9174e83c7345ddb227e5f90c72e469763
Gustavo Azevedo <ghca12@gmail.com>
2022-10-10 01:09:05 -0300

    first

--- script.js
+++ script.js
@@ -1,8 +1,62 @@
 const ninja = document.querySelector('.ninja');
+const caixa = document.querySelector('.caixa');
+const score = document.querySelector('.score');
+var cont = 0
+var jogar = false
+
+function jogue(){
+    return(
+        jogar = false
+    )
+       
+}
+
+document.addEventListener("keydown",(espaco) =>{
+    if((espaco.code === "Space")){
+        jump();
+    }
+})
 const jump = () => {
     ninja.classList.add('jump');
     setTimeout(() =>{
         ninja.classList.remove('jump');  
-    },650)
+    },600)
 }
-document.addEventListener('keydown', jump);
+
+const loop = setInterval(() => {
+
+    
+    const caixaPosicao = caixa.offsetLeft;
+    const ninjaPosicao = +getComputedStyle(ninja).bottom.replace('px','');
+     console.log(ninjaPosicao) 
+
+    if(caixaPosicao <= 90 && caixaPosicao > 0 && ninjaPosicao < 70 ){
+      
+        jogar = true
+        caixa.style.animation = 'none';
+        caixa.style.left = `${caixaPosicao}px`;
+
+        ninja.style.animation = 'none';
+        ninja.style.bottom  = `${ninjaPosicao}px` 
+
+        ninja.src = './fotos/game-over-2.gif'
+        
+        
+        clearInterval(loop)
+        
+       function gameover(){
+        alert('gameover') 
+        jogar = false
+       }
+       setTimeout(gameover,100)
+       setInterval(loop)
+     
+    }
+    else{
+        
+        cont++; //pontuacao por tempo
+        score.innerHTML = `SCORE: ${cont}`
+    }
+
+},10)
+
